25b8c4425a0849c89020eba0a5e555e4c36b96cf
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2021-05-15 22:34:23 -0300

    Update remove-server.sh

--- remove-server.sh
+++ remove-server.sh
@@ -9,3 +9,8 @@ sudo rm -rf /etc/mysql
 sudo apt-get remove --purge phpmyadmin*
 sudo apt-get autoremove
 sudo apt-get autoclean
+
+sudo rm -rf /etc/php /usr/share/php /usr/bin/php
+sudo rm -rf /usr/sbin/apache2 /usr/lib/apache2 /usr/share/apache2 /usr/share/man/man8/apache2.8.gz
+
+sudo apt-get remove --purge phpmyadmin* mysql* php*
