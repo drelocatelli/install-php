4c457a9711b3d89b21249712ba2941cc7b511118
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2021-05-18 12:04:15 -0300

    Update install-xampp-php7.4.sh

--- install-xampp-php7.4.sh
+++ install-xampp-php7.4.sh
@@ -1,4 +1,4 @@
-curl https://www.apachefriends.org/xampp-files/7.4.19/xampp-linux-x64-7.4.19-0-installer.run -o xampp.run
+wget -O xampp.run https://www.apachefriends.org/xampp-files/7.4.19/xampp-linux-x64-7.4.19-0-installer.run
 sudo chmod +x xampp.run
 sudo ./xampp.run
 sudo chmod 777 /opt/lampp/htdocs -R
