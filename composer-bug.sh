2390c4281624052b179796b260655937fb9b6f0b
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2021-06-11 13:20:21 -0300

    Update and rename composer-bug.sh to composerBug-install_laravel.sh

--- composer-bug.sh
+++
@@ -1,1 +0,0 @@
-php -r "readfile('https://getcomposer.org/installer');" | php && clear && php composer.phar
---
+++ composerBug-install_laravel.sh
@@ -0,0 +1,1 @@
+php -r "readfile('https://getcomposer.org/installer');" | php && clear && php composer.phar create-project --prefer-dist laravel/installer projectName
