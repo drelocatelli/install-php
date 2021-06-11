35bdc58a8bcbf063db61374e7dcada10444d8d8e
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2021-06-11 13:42:10 -0300

    Update install_laravel.sh

--- install_laravel.sh
+++ install_laravel.sh
@@ -1,1 +1,3 @@
+sudo apt install php-json php-iconv php-phar
+file="/opt/lampp/etc/php.ini"; echo -n `sudo sed -i 's/\;extension/extension/g' $file`; echo "todas extensões foram habilitadas de '$file'."
 php -r "readfile('https://getcomposer.org/installer');" | php && clear && php composer.phar create-project --prefer-dist laravel/installer projectName
